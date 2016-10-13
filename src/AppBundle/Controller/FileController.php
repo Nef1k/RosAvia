e8d7dc6ff41262ac1f14f7e47fceff7c158ecdd8
Zzzkvidi4 <zzzkvidi4@gmail.com>
2016-10-13 22:43:20 +0300

    Added FileController.php to collect actions with files

---
+++ src/AppBundle/Controller/FileController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 13.10.2016
+ * Time: 22:37
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FileController extends Controller
+{
+
+}
