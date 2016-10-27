41a7e28f34fbfe5934a9ab6b0bd3d2a606ec8d19
RedSkyDragon <redxsky59@gmail.com>
2016-10-27 17:31:58 +0300

    Fixed showing information in user edition

--- web/js/user_edit.js
+++ web/js/user_edit.js
@@ -3,7 +3,38 @@
  */
 
 function getParams() {
-    console.log($("#userGroup").attr("value"));
+    var userID = $("h1").attr("data-ID_user");
+    console.log(userID);
+    var user_group_id = $("#userGroup").val();
+    jQuery.get("/admin/user_group_edit?user_id="+userID+"&user_group_id="+user_group_id, function (data) {
+        $(".loader").addClass("hidden");
+        console.log(data);
+        data.params.forEach(function (item, i) {
+            if (!item.value){
+                item.value = ""
+            }
+            if (!(i%2)){
+                $("#params").append("<div class='row'>");
+            }
+
+            $("#params .row:last").append(
+                "<div class='col-md-6 text-center'>" +
+                    "<div class='input-group' id='+ item.id+'>"+
+                        "<span class='input-group-addon' id='basic-addon3'><b>"+item.name+":</b></span>"+
+                        "<input type='text' class='form-control' aria-label='...' value='"+ item.value+"'>"+
+                    "</div>" +
+                "</div>"
+            );
+            if (i%2){
+                $("#params").append("</div><br>");
+            }
+        });
+        $("#params br:last").remove(0);
+    })
+}
+
+function SaveChanges() {
+    
 }
 
 $(document).ready(function (event) {
