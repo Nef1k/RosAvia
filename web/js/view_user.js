c0bec6e478950542e7a4220d6d59ab6998ac1f6a
RedSkyDragon <redxsky59@gmail.com>
2016-11-04 18:18:32 +0300

    Added post request.

--- web/js/view_user.js
+++ web/js/view_user.js
@@ -1,3 +1,18 @@
-$(document).ready(function (event) {
+function GetUserFiles(){
+    var user_id=$("#user_files_table").attr("data-user_id");
+    console.log(user_id);
+}
 
+function AddFile() {
+    var user_id=$("#user_files_table").attr("data-user_id");
+    var description=$("#description").val();
+    var file=$("#InputFile").val();
+    console.log(user_id, description, file);
+    jQuery.post("/files/file_set", { user_id: user_id, display_name: description, userile: file}, function (data) {
+        console.log(data);
+    })
+}
+
+$(document).ready(function (event) {
+    GetUserFiles();
 });
