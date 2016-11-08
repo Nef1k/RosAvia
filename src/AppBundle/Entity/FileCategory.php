70c30d28b16391273eaba93798c4988940362e10
Nef1k <nef1k@outlook.com>
2016-11-08 11:15:02 +0300

    Regenerated doctrine entities

--- src/AppBundle/Entity/FileCategory.php
+++ src/AppBundle/Entity/FileCategory.php
@@ -26,4 +26,52 @@ class FileCategory
      * @ORM\Column(name="CategoryName", type="string")
      */
     private $CategoryName;
+
+    /**
+     * Set iDCategory
+     *
+     * @param integer $iDCategory
+     *
+     * @return FileCategory
+     */
+    public function setIDCategory($iDCategory)
+    {
+        $this->ID_Category = $iDCategory;
+
+        return $this;
+    }
+
+    /**
+     * Get iDCategory
+     *
+     * @return integer
+     */
+    public function getIDCategory()
+    {
+        return $this->ID_Category;
+    }
+
+    /**
+     * Set categoryName
+     *
+     * @param string $categoryName
+     *
+     * @return FileCategory
+     */
+    public function setCategoryName($categoryName)
+    {
+        $this->CategoryName = $categoryName;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryName
+     *
+     * @return string
+     */
+    public function getCategoryName()
+    {
+        return $this->CategoryName;
+    }
 }
