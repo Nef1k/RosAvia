73190b69eaa6ee47cabb72ef9cf87e4f5d2eb611
RedSkyDragon <redxsky59@gmail.com>
2016-11-09 19:44:27 +0300

    Added post request.

--- web/js/view_user.js
+++ web/js/view_user.js
@@ -1,6 +1,13 @@
 function GetUserFiles(){
-    var user_id=$("#user_files_table").attr("data-user_id");
+    var user_id=$("#user_id").val();
     console.log(user_id);
+    var field_names = JSON.stringify(["display_name", "file_type", "file_name"]);
+    var criteria = JSON.stringify({ "ID_User": user_id });
+    var sort = JSON.stringify({});
+    jQuery.post("/files/select",{field_names: field_names, criteria: criteria, sort: sort}, function (data) {
+        console.log(data);
+        $(".loader").addClass("hidden");
+    })
 }
 
 function AddFile() {
