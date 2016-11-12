93c642fdc62824e9a5e67da198fb7aaf7cdc7ec0
RedSkyDragon <redxsky59@gmail.com>
2016-11-12 15:21:33 +0300

    Blocked opportunity to save null mentor.

--- web/js/user_edit.js
+++ web/js/user_edit.js
@@ -42,20 +42,6 @@ function getParams() {
 }
 
 function SaveChanges() {
-    var user_id = parseInt($("h1").attr("data-ID_user"));
-    var mainFields ={
-        username: $("#username").val(),
-        email: $("#email").val(),
-        is_activated: $("#status").val(),
-        group_id: $("#userGroup").val(),
-        mentor_id: $("#mentor").val()
-    };
-    var addFields =[];
-    var sel = $(".addfields");
-    for (var i=0; i<sel.length; ++i ){
-        addFields[i] = {id: $(".addfields").eq(i).attr("data-id_field"), value: $(".addfields").eq(i).val()}
-    }
-
     var yesNoDialog = new YesNoDialog;
     yesNoDialog.setModalSelector("#yes-no-modal");
     yesNoDialog.show({
@@ -66,23 +52,44 @@ function SaveChanges() {
     });
     yesNoDialog.showLoader();
     var msg = "Данные изменены.";
-
-    var postParams = {
-        user_id: user_id,
-        general_settings: JSON.stringify(mainFields),
-        additional_settings: JSON.stringify(addFields)
-    };
-
-    jQuery.post("/admin/user_insert", postParams, function (data) {
-        console.log(data);
-        var errors = data.error_msg;
-        if (errors.length > 0){
-            msg = "Возникли следующие ошибки: <br>" + errors.join(" <br>") + ". Повторите ввод";
-        }
+    if ($("#mentor").val() == "null"){
+        msg = "Ментор не указан";
         yesNoDialog.message = msg;
         yesNoDialog.hideLoader();
         yesNoDialog.applyParams();
-    })
+    }
+    else{
+        var user_id = parseInt($("h1").attr("data-ID_user"));
+        var mainFields ={
+            username: $("#username").val(),
+            email: $("#email").val(),
+            is_activated: $("#status").val(),
+            group_id: $("#userGroup").val(),
+            mentor_id: $("#mentor").val()
+        };
+        var addFields =[];
+        var sel = $(".addfields");
+        for (var i=0; i<sel.length; ++i ){
+            addFields[i] = {id: $(".addfields").eq(i).attr("data-id_field"), value: $(".addfields").eq(i).val()}
+        }
+        var postParams = {
+            user_id: user_id,
+            general_settings: JSON.stringify(mainFields),
+            additional_settings: JSON.stringify(addFields)
+        };
+
+        jQuery.post("/admin/user_insert", postParams, function (data) {
+            console.log(data);
+            var errors = data.error_msg;
+            if (errors.length > 0){
+                msg = "Возникли следующие ошибки: <br>" + errors.join(" <br>") + ". Повторите ввод";
+            }
+            yesNoDialog.message = msg;
+            yesNoDialog.hideLoader();
+            yesNoDialog.applyParams();
+        })
+    }
+
 }
 
 function changeModalYesBtn (event) {
