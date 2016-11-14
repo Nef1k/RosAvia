5618fa96a357b92f60a7ebdfa133593345279805
Zzzkvidi4 <zzzkvidi4@gmail.com>
2016-11-14 20:41:29 +0300

    Fix missing statements in certificate pack Controller

--- src/AppBundle/Controller/CertificatePackController.php
+++ src/AppBundle/Controller/CertificatePackController.php
@@ -12,6 +12,8 @@ use AppBundle\Stuff\CertificatePackStuff;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class CertificatePackController extends Controller
 {
