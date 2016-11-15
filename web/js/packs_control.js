c6d2e1cddd1a113d1f1fa69d1ec4a98c7ed3ba2d
RedSkyDragon <redxsky59@gmail.com>
2016-11-15 11:45:47 +0300

    Added request.

--- web/js/packs_control.js
+++ web/js/packs_control.js
@@ -1,3 +1,16 @@
 /**
  * Created by 166878 on 15.11.2016.
  */
+
+function GetCertPacks() {
+    $("user_packs").remove();
+    var field_names = JSON.stringify(["ID_Sertificate", "name", "last_name", "phone_number", "flight_type", "price"]);
+    jQuery.post("/certificate_pack/select", {field_names:field_names}, function (data) {
+        $(".pack_loader").addClass("hidden");
+        console.log(data)
+    });
+}
+
+$(document).ready(function (event) {
+    GetCertPacks();
+});
